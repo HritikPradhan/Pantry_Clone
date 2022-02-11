3913b5b5e00e61a4c89ec5d232bdf60b8f642754
Hritik Pradhan <93572272+HritikPradhan@users.noreply.github.com>
2022-02-11 19:14:48 +0530

    Update basketModel.js

--- src/Model/basketModel.js
+++ src/Model/basketModel.js
@@ -5,8 +5,7 @@ const basketSchema = new mongoose.Schema({
     basketName: { type: String },
     basket: { type: mongoose.Schema.Types.Mixed },
     pantryId: { type: mongoose.Schema.Types.ObjectId, ref: "pantry" },
-    //expire: {type:String,default:"Your Basket Will Be Deleted Automatically After 24hr"}
-    expire:{type:Number,default:86400}
+    expire: {type:String,default:"Your Basket Will Be Deleted Automatically After 24hr"}
 },{timestamps:true});
 
 basketSchema.index({createdAt: 1},{expireAfterSeconds: 86400});
